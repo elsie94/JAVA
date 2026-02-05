b63d447556d23863a40c00a79cf291fd2700956f
elsie94 <elsie9421@gmail.com>
2026-02-05 03:00:57 -0800

    first commit

---
+++ ReportCard.java
@@ -0,0 +1,18 @@
+/*  Inside main(), complete the following steps: Declare an int named studentID and assign it a value
+Declare a double named score and assign it a value (use a decimal), 
+Declare a char named grade and assign it a single character (in single quotes), 
+Print all three values  */
+
+public class ReportCard{
+    public static void main(String[] args){
+        //Declaring variables and assigning values
+        int studentID = 1000167;
+        double score = 93.5;
+        char grade = 'A';
+
+        //Printing the values
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Score: " + score);
+        System.out.println("Grade: " + grade);
+    }
+}
