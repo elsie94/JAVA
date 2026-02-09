4aaa1f248a40bc0f2e850d70522707dfad49435b
elsie94 <elsie9421@gmail.com>
2026-02-09 10:55:11 +0100

    first commit

---
+++ Operators.java
@@ -0,0 +1,18 @@
+/*
+Inside main(), complete the following steps:
+Declare two int variables named itemPrice and shippingCost, and assign them values
+Create an int variable named sum
+Calculate the total cost by adding itemPrice and shippingCost (store the result in sum)
+Print the total cost (sum)
+ */
+
+public class Operators{
+    public static void main(String[] args){
+int itemPrice = 100;
+int shippingCost = 50;
+
+int sum = itemPrice + shippingCost;
+System.out.println("The Total Cost is: " + sum);
+
+}
+}
