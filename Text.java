a2fcd38f3a055c37d3d51598c6497d998ea4deb2
elsie94 <elsie9421@gmail.com>
2026-02-10 10:38:45 +0100

    first commit

---
+++ Text.java
@@ -0,0 +1,13 @@
+/* 
+Inside main(), complete the following steps:
+Create a String variable named message and assign it a value
+Print the message
+Note: Strings must be written inside double quotes.
+ */
+
+public class Text{
+    public static void main(String[] args){
+        String message = "I am hungry.";
+        System.out.println(message);
+    }
+}
